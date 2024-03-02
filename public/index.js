982c829d0262615eea35c82ad0a428c745109533
AndreTaq <andretaqian@gmail.com>
2024-03-02 14:38:32 -0800

    added fetch and installed chartJS

--- public/index.js
+++ public/index.js
@@ -4,6 +4,25 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    // const symbols = ['GME', 'DIS', 'MSFT', 'BNTX'];
+
+    // const response = await fetch(`https://api.twelvedata.com/symbol_search?symbol=${symbols}`);
+    // const responseObject = await response.json();
+    // console.log(responseObject);
+
+
+    // const apiKey = '08409817f62d403fbb7f98e4b87cd86e';
+    const symbols = ['GME', 'MSFT', 'DIS', 'BNTX'];
+    const symbolsQueryString = symbols.join(',');
+
+    const url = `https://api.twelvedata.com/time_series?symbol=${symbolsQueryString}&interval=1day&apikey=${apiKey}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data);
+
+   
 }
 
 main()
